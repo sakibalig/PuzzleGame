97d95c7ebfc5b7ee476013a69bf10787bc5240c5
Shravan Kumar Singh <108937307+Hurricane0017@users.noreply.github.com>
2022-07-20 22:50:22 +0530

    corrected previous best count error

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.getElementById('puzzle-container').onclick=function(){
                 document.getElementById('puzzle-container').style.borderRadius='2em';
                 document.getElementById('puzzle-container').style.opacity=0.45;
                 clearInterval(myTimer);
-                if(document.getElementById('record-count').innerHTML<movesCount){
+                if(document.getElementById('record-count').innerHTML>movesCount){
                     document.getElementById('record-count').innerHTML=movesCount;
                 }
             }
